1a0fd085bce390c29223a50a58ae2c43bf9071b7
Kanchan-Nazare <160421893+Kanchan-Nazare@users.noreply.github.com>
2024-04-04 01:28:50 +0530

    Flask_Project_predictive_analysis-on IRIS Dataset
    Here,above Predictive model based on the IRIS dataset included with the deployment using Flask webframework to converting model into json;Test of model through "POST" method (Snap mentioned) rendering to prediction of label based on that (snap added) here added.

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request
+import sklearn
+import pickle
+import sys
+print(sys)
+
+app= Flask(__name__)
+@app.route('/')
+
+
+def index():
+    return render_template('iris.html')
+
+@app.route('/get_data', methods = ['POST'])
+def model_prediction():
+    data=request.form
+    print(data)
+    load_model=pickle.load(open('iris.pkl','rb'))
+    print(load_model)
+
+
+    user_data=[[float(data['x1_sepal_length']),
+                float(data['x2_sepal_width']),
+                float(data['x3_petal_length']),
+                float(data['x4_petal_width'])
+                ]]
+    print(user_data)
+
+    result=load_model.predict(user_data)
+    print(result)
+
+    target=['setosa', 'versicolor', 'virginica']
+
+    print(f"Prediction= {target[result[0]]}")
+
+    return target[result[0]]
+
+app.run(debug=True)
+    
